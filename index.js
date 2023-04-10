197b55a24bd203d5cc1e167ab9f7b70b8c491d6c
Penelope Drago <prd@drago.com.au>
2023-04-10 14:06:22 +1000

    Revert "add event listener"
    This reverts commit 3cd820707bbccc85d831a4566667d5b3bf24c5ac.

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 function addingEventListener() {
-  const input = document.getElementById('button');
-
-  function clickAlert() {
-    alert('I was clicked!');
-  }
-
-  input.addEventListener('click', clickAlert);
 }
-
-addingEventListener();
